e20fb01487da32f4693260ba25a6511eaec0de14
Federico Fantacone <fede26f@hotmail.com>
2022-11-25 15:58:48 -0300

    con DB

---
+++ PhotoSharingApplication/Data/PhotoDBContext.cs
@@ -0,0 +1,18 @@
+﻿using PhotoSharingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Data
+{
+    public class PhotoDBContext : DbContext
+    {
+        public PhotoDBContext(): base ("keyPhotoDB") { }
+
+        public DbSet<Photo> photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
